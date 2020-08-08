085a636b4039427c9ca0be4901fd3adce38333de
Pedro Guillermo Rodríguez <20155001@ce.pucmm.edu.do>
2020-08-08 17:51:45 -0400

    Updated user-profile

--- user-profile/nodejs/index.js
+++ user-profile/nodejs/index.js
@@ -12,6 +12,30 @@ exports.handler = function(event, context, callback) {
 
     var secretBuffer = new Buffer(process.env.AUTH0_SECRET);
     jwt.verify(token, secretBuffer, function(err, decoded) {
-        
-    })
+        if(err){
+            console.log('Failed jwt verification: ', err, 'auth: ', event.authToken);
+            callback('Authorization Failed');
+        }
+        else {
+            var body = {
+                'id_token': token
+            };
+
+            var options = {
+                url = 'https://' + process.env.DOMAIN + '/tokeninfo',
+                method: 'POST',
+                json: true,
+                body: body
+            };
+
+            request(options, function(error, response, body){
+                if (!error && response.statusCode === 200) {
+                    callback(null, body);
+                }
+                else {
+                    callback(error);
+                }
+            });
+        }
+    });
 };
