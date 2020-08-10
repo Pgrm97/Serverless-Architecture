a9c8ec5c202ac2474cd0e787b6e74eb4514aec6f
Pedro Guillermo Rodríguez <20155001@ce.pucmm.edu.do>
2020-08-09 23:27:59 -0400

    Updated Transcode function

--- transcode-video/nodejs/index.js
+++ transcode-video/nodejs/index.js
@@ -1,5 +1,6 @@
 'use strict';
 var AWS = require('aws-sdk');
+var firebase = require('firebase');
 
 var elasticTranscoder = new AWS.ElasticTranscoder({
     region: 'us-east-1',
@@ -7,8 +8,31 @@ var elasticTranscoder = new AWS.ElasticTranscoder({
     secretAccessKey: 'w32XNun0oG1aCOdagIEDCy8Oi0oHpOOjbeEt4DTz'
   });
 
+//Función necesaria para correr Firebase.
+firebase.initializeApp({
+    serviceAccount: process.env.SERVICE_ACCOUNT,
+    databaseURL: process.env.DATABASE_URL
+});
+
+//Esta función se encarga de introducir una entrada en Firebase del video transcodeado.
+function pushVideoEntryToFirebase(key, callback) {
+    console.log('Adding video entry to firebase at key: ', key);
+
+    //Raíz de la base de datos en Firebase.
+    var database = firebase.database().ref();
+
+    database.child('videos').child(key).set({
+        transcoding: true
+    }).then(function () {
+        callback(null, 'Video record saved to firebase');
+    }).catch(function(err){
+        callback(err);
+    })
+}
+
 exports.handler = function(event, context, callback) {
-    console.log('Welcome');
+    //Suspende la función cuando se invoca un callback.
+    context.callbackWaitsForEmptyEventLoop = false;
 
     var key = event.Records[0].s3.object.key;
 
@@ -40,7 +64,12 @@ exports.handler = function(event, context, callback) {
     
     elasticTranscoder.createJob(params, function(error, data){
         if (error){
+            console.log('Error creating Elastic Transcoder job.');
             callback(error);
+            return;
         }
+
+        console.log("Transcoded Successful!");
+        pushVideoEntryToFirebase(outputKey, callback);
     });
 };
