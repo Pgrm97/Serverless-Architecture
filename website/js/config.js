b39fff9c5f2b176d65bcf7d26fe8f9385ccf45f5
Sebastián Jorge Cruz <sebastianjorgecruz25@gmail.com>
2020-08-10 01:11:46 -0400

    Added firebase to front-end

--- website/js/config.js
+++ website/js/config.js
@@ -1,7 +1,11 @@
 var configConstants = {
-	auth0: {
-		domain: 'tigredeweb.us.auth0.com',
-		clientId: 'bWHQE7E9eDBQxXpbjQi59f5X5GGIqAx5'
-	},
-	apiBaseUrl: 'https://94jahhku2j.execute-api.us-east-1.amazonaws.com/dev'
+  auth0: {
+    domain: "tigredeweb.us.auth0.com",
+    clientId: "bWHQE7E9eDBQxXpbjQi59f5X5GGIqAx5",
+  },
+  firebase: {
+    apiKey: "AIzaSyDhRZEcegIVWiXiqlUCZ7shRY6wmc49fo0",
+    databaseURL: "https://hour-video-153f5.firebaseio.com/",
+  },
+  apiBaseUrl: "https://94jahhku2j.execute-api.us-east-1.amazonaws.com/dev",
 };
